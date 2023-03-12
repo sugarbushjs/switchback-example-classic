c29ac5b06e6a72dcf407bad0afe631b6d3ff395c
Steven Wickers <stevenwickers@gmail.com>
2023-03-12 16:16:14 -0400

    init commit

---
+++ src/dispatchers/index.ts
@@ -0,0 +1,6 @@
+import { adpStore } from '../components/App'
+import { store } from '../components/App/store'
+import { adaptiveDispatch } from 'sugarbush'
+
+export const SystemDispatch = () => adpStore.dispatch('SystemState')
+export const CounterDispatch = () => adaptiveDispatch({ dispatch: store.dispatch, key: 'CounterState', suppressLogging: true})
